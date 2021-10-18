b65319c2d2908f396a3f64960718ac99fa8f7b86
Bruno Bzeznik <Bruno.Bzeznik@univ-grenoble-alpes.fr>
2021-10-18 07:58:49 +0200

    siesta: openmpi3 , osu-micro-benchmarks

--- pkgs/beagle/default.nix
+++ pkgs/beagle/default.nix
@@ -1,4 +1,5 @@
 { stdenv, fetchFromGitHub, autoconf, automake, pkg-config, libtool, openjdk }:
+#{ stdenv, fetchFromGitHub, autoconf, automake, pkg-config, libtool, openjdk, ocl-icd, intel-ocl, opencl-headers }:
 
 stdenv.mkDerivation rec {
   version = "3.1.2"; 
@@ -11,11 +12,17 @@ stdenv.mkDerivation rec {
     sha256 = "06r3068y832y0b2pc7kdswvs65i5809ampmsb8mrrw6nb338dd9j";
   };
 
-  nativeBuildInputs = [ autoconf automake pkg-config libtool openjdk ];
+ nativeBuildInputs = [ autoconf automake pkg-config libtool ];
+ buildInputs = [ openjdk ];
+ # nativeBuildInputs = [ autoconf automake pkg-config libtool opencl-headers ];
+ # buildInputs = [ intel-ocl ocl-icd openjdk ];
 
   preConfigure = ''
     ./autogen.sh   
   '';              
+
+#  configureFlags = [ "--with-cuda=${cudatoolkit}" "--with-opencl=${intel-ocl}" ];
+#   configureFlags = [ "--with-opencl=${intel-ocl}" ];
 
   meta = {
      description = "high-performance library that can perform the core calculations at the heart of most Bayesian and Maximum Likelihood phylogenetics packages";
