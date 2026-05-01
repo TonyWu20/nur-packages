639be71bf7ffab17c4c5bb371018bce365fa7500
TonyWu20 <tony.w21@gmail.com>
2026-05-01 23:22:55 +0800

    Update flake inputs

--- pkgs/nvidia/nvhpc.nix
+++ pkgs/nvidia/nvhpc.nix
@@ -4,11 +4,15 @@
 , autoPatchelfHook
 , glibc
 , glib
+, gmp
+, gdk-pixbuf
 , mpi
 , autoAddDriverRunpath
 , libxml2
-, ncurses
-, gmp
+, ncurses5
+, expat
+, libkrb5
+, libxkbcommon
 , python312
 , python311
 , python310
@@ -17,6 +21,28 @@
 , mesa
 , wayland
 , libxcb
+, xorg
+, pulseaudio
+, krb5
+, gtk2
+, fontconfig
+, freetype
+, numactl
+, nss
+, unixODBC
+, alsa-lib
+, libglvnd
+, libtiff
+, qt6Packages
+, rdma-core
+, ucx
+, gst_all_1
+, curlMinimal
+, qt6
+, libxcrypt-legacy
+, ncurses6
+, patchelf
+, gcc-unwrapped
 }:
 stdenv.mkDerivation rec {
   name = "nvidia-hpc-sdk-${version}";
@@ -41,46 +67,95 @@ stdenv.mkDerivation rec {
     python311
     python310
     libxml2
-    ncurses
+    ncurses5
+    xorg.libX11
+    xorg.libXtst
+    xorg.libXrender
+    xorg.libXt
+    xorg.libXtst
+    xorg.libXi
+    xorg.libXext
+    xorg.libXdamage
+    xorg.libxcb
+    xorg.xcbutilimage
+    xorg.xcbutilrenderutil
+    xorg.xcbutilwm
+    xorg.xcbutilkeysyms
+    expat
+    pulseaudio
+    libxkbcommon
+    libkrb5
+    krb5
+    gtk2
+    fontconfig
+    freetype
+    numactl
+    nss
+    unixODBC
+    alsa-lib
+    libglvnd
+    (lib.getLib libtiff)
+    libtiff
+    qt6Packages.qtwayland
+    rdma-core
+    (ucx.override { enableCuda = false; })
+    xorg.libxshmfence
+    xorg.libxkbfile
     openssl
     libibmad
     libibumad
     wayland
     mesa
     libxcb
-  ];
+    (map lib.getLib ([
+      curlMinimal
+      gst_all_1.gstreamer
+      gst_all_1.gst-plugins-base
+    ]))
+    libxcrypt-legacy
+    ncurses6
+  ] ++ (with qt6;[
+    qtmultimedia
+    qttools
+    qtpositioning
+    qtscxml
+    qtsvg
+    qtwebchannel
+    qtwebengine
+  ]);
   # propagatedBuildInputs = [ glibc glib fftw fftwQuad fftwFloat fftwLongDouble fftwMpi openssl ];
   dontConfigure = true;
   dontBuild = true;
   phases = [ "unpackPhase" "patchPhase" "installPhase" "fixupPhase" ];
 
-  libPath = lib.makeLibraryPath [
-    mpi
-    stdenv.cc.cc.lib
-    glibc
-    glib
-    gmp
-    ncurses
-    python312
-    python311
-    python310
-    libxml2
-    openssl
-    libibmad
-    libibumad
-  ];
   autoPatchelfIgnoreMissingDeps = [
-    "libpython3.9.so.1.0"
     "libpython3.8.so.1.0"
-    "libibverbs.so.1"
-    "libcrypto.so.1"
-    "*"
+    "libpython3.9.so.1.0"
+    "libcuda.so.1"
+    "libnvidia-ml.so.1"
+    "libcom_err.so.2"
+    "libxpmem.so.0"
+    "libgdrapi.so.2"
+    "libtiff.so.5"
   ];
 
   patchPhase = ''
     patchShebangs  --build ./install
     patchShebangs --build install_components/install*
   '';
+
+  runtimeDependencies = [
+    "${lib.getLib gcc-unwrapped.lib}"
+    "${lib.getLib gcc-unwrapped.libgcc}"
+    "${lib.getLib glib}"
+    (placeholder "lib")
+    (placeholder "out")
+    "${placeholder "out"}/cuda/nvvm"
+    "${lib.getLib stdenv.cc.cc}/lib64"
+    "${placeholder "out"}/cuda/lib64"
+    "${placeholder "out"}/math_libs/lib64"
+    "${placeholder "out"}/cuda/12.9/nvvm/lib64"
+  ];
 
   installPhase = ''
         mkdir -p $out
@@ -95,6 +170,14 @@ stdenv.mkDerivation rec {
     	  exit 1
         fi
   '';
+  preFixup = ''
+    ${lib.getExe' patchelf "patchelf"} $out/lib64/libnvrtc.so --add-needed libnvrtc-builtins.so
+  '';
+
+  postInstall = ''
+    find $out -name '*python3.8*' -delete
+    find $out -name '*python3.9*' -delete
+  '';
 
   fixupPhase = ''
     # Patch ELF binaries
@@ -102,7 +185,6 @@ stdenv.mkDerivation rec {
     # find $out -type f \( -executable -o -name "*.so*" \) -print0 | while IFS= read -r -d $'\0' file; do
     #   if isELF "$file"; then
     #     echo "Patching $file"
-    #     patchelf --add-rpath "${libPath}:$out/${platform}/${version}/cuda/12.9/targets/x86_64-linux/lib/" "$file">/dev/null || true
     #   fi
     # done
     patchShebangs --host $out/${platform}/${version}/compilers/bin/makelocalrc
